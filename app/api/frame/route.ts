7d661a537f374c8b3b7fd7dd397ce5108f19d63d
Gabriel Temtsen <gabrieltemtsen@gmail.com>
2024-06-07 21:54:38 +0100

    added frame

--- app/api/frame/route.ts
+++ app/api/frame/route.ts
@@ -1,24 +1,23 @@
 import { NextRequest, NextResponse } from 'next/server';
 
 async function getResponse(req: NextRequest): Promise<NextResponse> {
-  const searchParams = req.nextUrl.searchParams
-  const id:any = searchParams.get("id")
-  const idAsNumber = parseInt(id)
+  const searchParams = req.nextUrl.searchParams;
+  const id: any = searchParams.get('id');
+  const idAsNumber = parseInt(id);
 
-  const nextId = idAsNumber + 1
+  const nextId = idAsNumber + 1;
 
-  if(idAsNumber === 1){
-      return new NextResponse(`<!DOCTYPE html><html><head>
+  if (idAsNumber === 1) {
+    return new NextResponse(`<!DOCTYPE html><html><head>
     <title>This is me</title>
     <meta property="fc:frame" content="vNext" />
     <meta property="fc:frame:image" content="${process.env.NEXT_PUBLIC_GATEWAY_URL}/ipfs/QmV1JEbHELKkyve1Cn5yAxUdkwWENnRuNr1gAJ2oGqaP5g" />
     <meta property="fc:frame:button:1" content="Tip me" />
-    <meta property="fc:frame:button:2" content="Follow me" />
-    <meta property="fc:frame:button:2:action" content="post_redirect" />
-    <meta property="fc:frame:post_url" content="${process.env.NEXT_PUBLIC_BASE_URL}/api/end" />
-  </head></html>`)
+    <meta property="fc:frame:button:1:action" content="post_redirect" />
+    <meta property="fc:frame:post_url" content="${process.env.NEXT_PUBLIC_BASE_URL}/api/validate" />
+  </head></html>`);
   } else {
-  return new NextResponse(`<!DOCTYPE html><html><head>
+    return new NextResponse(`<!DOCTYPE html><html><head>
     <title>This is frame ${id}</title>
     <meta property="fc:frame" content="vNext" />
     <meta property="fc:frame:image" content="${process.env.NEXT_PUBLIC_GATEWAY_URL}/ipfs/QmPGVGuJBWfbFSggnGEx6pehsGqXwxGYxxGkTTSwDxncJc" />
--- app/api/validate/route.ts
+++ app/api/validate/route.ts
@@ -1,23 +1,15 @@
 import { NextRequest, NextResponse } from 'next/server';
 
-async function getResponse(req: NextRequest): Promise<NextResponse> {
-  const user = req.body;
-  const searchParams = req.nextUrl.searchParams
-  const id:any = searchParams.get("id")
-  const idAsNumber = parseInt(id)
+export async function POST(req: NextRequest): Promise<NextResponse> {
+  const data = await req.json();
+  console.log(data);
 
- 
-  return new NextResponse(`<!DOCTYPE html><html><head>
-    <title>This is a frame thanks ${user} </title>
-    <meta property="fc:frame" content="vNext" />
-    <meta property="fc:frame:image" content="${process.env.NEXT_PUBLIC_GATEWAY_URL}/ipfs/QmPGVGuJBWfbFSggnGEx6pehsGqXwxGYxxGkTTSwDxncJc" />
-    <meta property="fc:frame:button:1" content="${idAsNumber}" />
-  </head></html>`);
-  
-}
+  const newFrameImageUrl = `${process.env.NEXT_PUBLIC_GATEWAY_URL}/ipfs/QmPGVGuJBWfbFSggnGEx6pehsGqXwxGYxxGkTTSwDxncJc`;
 
-export async function POST(req: NextRequest): Promise<Response> {
-  return getResponse(req);
+  return NextResponse.json({
+    success: true,
+    frameImage: newFrameImageUrl,
+  });
 }
 
 export const dynamic = 'force-dynamic';
