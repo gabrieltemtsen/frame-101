20b00904e0dc25bf8feb47c9932514d1366b1f22
Gabriel Temtsen <gabrieltemtsen@gmail.com>
2024-06-08 15:24:09 +0100

    fix

--- app/frame/route.ts
+++ app/frame/route.ts
@@ -24,10 +24,10 @@ export async function GET(req: NextRequest, res: NextResponse) {
 
 export async function POST(req: NextRequest, res: NextResponse) {
     try {
-    //   const body = await req.json();
-    //   const fid = body.untrustedData.fid;
+      const body = await req.json();
+      const fid = body.untrustedData.fid;
   
-    //   const { isValid, message } = await fdk.validateFrameMessage(body);
+      const { isValid, message } = await fdk.validateFrameMessage(body);
   
       const frameMetadata = await fdk.getFrameMetadata({
         post_url: `${process.env.NEXT_PUBLIC_BASE_URL}/redirect`,
@@ -38,12 +38,12 @@ export async function POST(req: NextRequest, res: NextResponse) {
         cid: "QmPGVGuJBWfbFSggnGEx6pehsGqXwxGYxxGkTTSwDxncJc",
       });
   
-    //   if (isValid) {
-    //     await fdk.sendAnalytics("frame-mint-tutorial-mint", body);
-    //   }
+      if (isValid) {
+        await fdk.sendAnalytics("frame-mint-tutorial-mint", body);
+      }
   
-      return NextResponse.json({ success: true });
-    } catch (error: any) {
+      return new NextResponse(frameMetadata);
+        } catch (error: any) {
       console.error(error);
       return NextResponse.json({ success: false, error: error.message });
     }
