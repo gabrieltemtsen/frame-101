5d66173ec83d878190e16ccb14189162dd20caee
Gabriel Temtsen <gabrieltemtsen@gmail.com>
2024-06-08 14:50:55 +0100

    updt

--- next.config.mjs
+++ next.config.mjs
@@ -1,25 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    reactStrictMode: true,
-    async redirects(){
-      return [
-        {
-          source: '/memefordegen',
-          destination: 'https://cosmiccowboys.cloud',
-          permanent: false
-        },
-        {
-          source: '/pinatacloud',
-          destination: 'https://www.pinata.cloud/blog/how-to-make-a-frame-on-farcaster-using-ipfs',
-          permanent: false
-        },
-        {
-          source: '/video',
-          destination: 'https://youtu.be/wUt5NjXHSO4',
-          permanent: false
-        }
-      ]
-    }
-  };
-  
-  export default nextConfig;
+const nextConfig = {};
+
+export default nextConfig;
